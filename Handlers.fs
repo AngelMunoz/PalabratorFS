d30ab2cea17761024ae47f58f942f07e5773edd2
Angel D. Munoz <scythes.lol@gmail.com>
2021-01-08 00:18:33 -0700

    simplify logic branching add response helpers

--- Handlers.fs
+++ Handlers.fs
@@ -1,41 +1,44 @@
 namespace PalabratorFs.Handlers
 
+open Microsoft.AspNetCore.Authentication.JwtBearer
 open Microsoft.AspNetCore.Http
 open FSharp.Control.Tasks
 open Giraffe
 open Saturn.Auth
 
-open MongoDB.Bson
 open PalabratorFs
 open PalabratorFs.Database
 open System
 open System.Security.Claims
-open Saturn.Controller
+
+[<AutoOpen>]
+module private Responses =
+    type Response = { message: string }
+    let created<'T> (value: 'T) = Successful.CREATED value
+
+    let badRequest (msg: string) =
+        RequestErrors.BAD_REQUEST { message = msg }
+
+    let unauthorized (msg: string) =
+        RequestErrors.UNAUTHORIZED JwtBearerDefaults.AuthenticationScheme "http://localhost:5001" { message = msg }
+
+    let forbidden (msg: string) =
+        RequestErrors.FORBIDDEN { message = msg }
+
+    let unprocessableEntity (msg: string) =
+        RequestErrors.UNPROCESSABLE_ENTITY { message = msg }
 
 [<RequireQualifiedAccess>]
 module Auth =
     let Login =
         fun (next: HttpFunc) (ctx: HttpContext) ->
             task {
-                let! payload = Helpers.TryBindJsonAsync<LoginPayload>(ctx)
-
-                match payload with
-                | None ->
-                    return!
-                        (setStatusCode 400
-                         >=> json {| message = "Missing Login Info" |})
-                            next
-                            ctx
+                match! Helpers.TryBindJsonAsync<LoginPayload> ctx with
+                | None -> return! badRequest "Missing Login Info" next ctx
                 | Some payload ->
-                    let! canLogin = Users.VerifyPassword payload.email payload.password
-
-                    if not canLogin then
-                        return!
-                            (setStatusCode 401
-                             >=> json {| message = "Invalid Credentials" |})
-                                next
-                                ctx
-                    else
+                    match! Users.VerifyPassword payload.email payload.password with
+                    | false -> return! unauthorized "Invalid Credentials" next ctx
+                    | true ->
                         let claims =
                             [ Claim(ClaimTypes.Email, payload.email) ]
 
@@ -57,28 +60,17 @@ module Auth =
     let Signup =
         fun (next: HttpFunc) (ctx: HttpContext) ->
             task {
-                let! payload = Helpers.TryBindJsonAsync<SignupPayload>(ctx)
-
-                match payload with
-                | None ->
-                    return!
-                        (setStatusCode 400
-                         >=> json {| message = "Missing Signup Info" |})
-                            next
-                            ctx
+                match! Helpers.TryBindJsonAsync<SignupPayload> ctx with
+                | None -> return! badRequest "Missing Signup Info" next ctx
                 | Some payload ->
-                    let! exists = Users.Exists payload.email
-
-                    if exists then
-                        return!
-                            (setStatusCode 400
-                             >=> json {| message = "That email already exists" |})
-                                next
-                                ctx
-                    else
-                        let! created = Users.Create payload
-
-                        if created then
+                    match! Users.Exists payload.email with
+                    | true -> return! badRequest "That email already exists" next ctx
+                    | false ->
+                        match! Users.Create payload with
+                        | false ->
+                            return!
+                                unprocessableEntity "The email is available but we could not create the user" next ctx
+                        | true ->
                             let claims =
                                 [ Claim(ClaimTypes.Email, payload.email) ]
 
@@ -90,18 +82,11 @@ module Auth =
                                     claims
 
                             return!
-                                json
+                                created
                                     {| email = payload.email
                                        token = token |}
                                     next
                                     ctx
-                        else
-                            return!
-                                (setStatusCode 422
-                                 >=> json {| message = "The email is available but we could not create the user" |})
-                                    next
-                                    ctx
-
             }
 
 
@@ -111,15 +96,8 @@ module Profiles =
     let ProfilesList =
         fun (next: HttpFunc) (ctx: HttpContext) ->
             task {
-                let! user = Helpers.TryExtractUserFromRequest ctx
-
-                match user with
-                | None ->
-                    return!
-                        (setStatusCode 403
-                         >=> json {| message = "You don't have access to this resource" |})
-                            next
-                            ctx
+                match! Helpers.TryExtractUserFromRequest ctx with
+                | None -> return! forbidden "You don't have access to this resource" next ctx
                 | Some user ->
                     let (page, limit) = Helpers.ExtractPagination ctx
                     let! results = Profiles.Find user._id (Some page) (Some limit)
@@ -130,111 +108,61 @@ module Profiles =
     let Add =
         fun (next: HttpFunc) (ctx: HttpContext) ->
             task {
-                let! user = Helpers.TryExtractUserFromRequest ctx
-
-                match user with
-                | None ->
-                    return!
-                        (setStatusCode 403
-                         >=> json {| message = "You don't have access to this resource" |})
-                            next
-                            ctx
+                match! Helpers.TryExtractUserFromRequest ctx with
+                | None -> return! forbidden "You don't have access to this resource" next ctx
                 | Some user ->
-                    let! payload = Helpers.TryBindJsonAsync<ProfilePayload> ctx
-
-                    match payload with
-                    | None ->
-                        return!
-                            (setStatusCode 400
-                             >=> json {| message = "Missing Profile Information" |})
-                                next
-                                ctx
+                    match! Helpers.TryBindJsonAsync<ProfilePayload> ctx with
+                    | None -> return! badRequest "Missing Profile Information" next ctx
                     | Some payload ->
                         match! Profiles.Exists user._id payload.name with
-                        | true ->
-                            return!
-                                (setStatusCode 400
-                                 >=> json {| message = "That profile already exists" |})
-                                    next
-                                    ctx
+                        | true -> return! badRequest "That profile already exists" next ctx
                         | false ->
-                            let! result = Profiles.Create payload
-
-                            if result then
-                                let! profiles = Profiles.Find user._id None None
-                                return! (setStatusCode 201 >=> json profiles) next ctx
-                            else
+                            match! Profiles.Create payload with
+                            | false ->
                                 return!
-                                    (setStatusCode 422
-                                     >=> json {| message = "The profile name is available but we couldn't create it" |})
+                                    unprocessableEntity
+                                        "The profile name is available but we couldn't create it"
                                         next
                                         ctx
+                            | true ->
+                                let (page, limit) = Helpers.ExtractPagination ctx
+                                let! profiles = Profiles.Find user._id (Some page) (Some limit)
+                                return! created profiles next ctx
             }
 
     let Update =
         fun (next: HttpFunc) (ctx: HttpContext) ->
             task {
-                let! user = Helpers.TryExtractUserFromRequest ctx
-
-                match user with
-                | None ->
-                    return!
-                        (setStatusCode 403
-                         >=> json {| message = "You don't have access to this resource" |})
-                            next
-                            ctx
+                match! Helpers.TryExtractUserFromRequest ctx with
+                | None -> return! forbidden "You don't have access to this resource" next ctx
                 | Some user ->
                     match! Helpers.TryBindJsonAsync<Profile> ctx with
-                    | None ->
-                        return!
-                            (setStatusCode 400
-                             >=> json {| message = "The Profile is not present in the request" |})
-                                next
-                                ctx
+                    | None -> return! badRequest "The Profile is not present in the request" next ctx
                     | Some payload ->
-                        match! Profiles.Exists user._id payload.name with
-                        | true ->
-                            return!
-                                (setStatusCode 400
-                                 >=> json {| message = "The Profile name exists already" |})
-                                    next
-                                    ctx
-                        | false ->
-                            match! Profiles.Rename payload._id payload.name with
+                        if payload.owner <> user._id then
+                            return! forbidden "You don't have access to this resource" next ctx
+                        else
+                            match! Profiles.Exists user._id payload.name with
+                            | true -> return! badRequest "The Profile name exists already" next ctx
                             | false ->
-                                return!
-                                    (setStatusCode 422
-                                     >=> json {| message = "We were not able to rename the profile" |})
-                                        next
-                                        ctx
-                            | true -> return! json payload next ctx
+                                match! Profiles.Rename payload._id payload.name with
+                                | false -> return! unprocessableEntity "We were not able to rename the profile" next ctx
+                                | true -> return! json payload next ctx
             }
 
     let Delete =
         fun (next: HttpFunc) (ctx: HttpContext) ->
             task {
                 match! Helpers.TryExtractUserFromRequest ctx with
-                | None ->
-                    return!
-                        (setStatusCode 403
-                         >=> json {| message = "You don't have access to this resource" |})
-                            next
-                            ctx
+                | None -> return! forbidden "You don't have access to this resource" next ctx
                 | Some user ->
                     match! Helpers.TryBindJsonAsync<Profile> ctx with
-                    | None ->
-                        return!
-                            (setStatusCode 400
-                             >=> json {| message = "The Profile is not present in the request" |})
-                                next
-                                ctx
+                    | None -> return! badRequest "The Profile is not present in the request" next ctx
                     | Some payload ->
-                        match! Profiles.Delete payload._id payload.owner with
-                        | false ->
-                            return!
-                                (setStatusCode 422
-                                 >=> json {| message = "We were not able to delete this profile" |})
-                                    next
-                                    ctx
-                        | true -> return! setStatusCode 204 next ctx
+                        if payload.owner <> user._id then
+                            return! forbidden "You don't have access to this resource" next ctx
+                        else
+                            match! Profiles.Delete payload._id payload.owner with
+                            | false -> return! unprocessableEntity "We were not able to delete this profile" next ctx
+                            | true -> return! setStatusCode 204 next ctx
             }
